87613524c31a0a705463ca69f1cbe6fd8908c173
Frank Wei <frank.wei@mirana.tech>
2022-02-16 12:06:27 +0800

    feat(UI): customized `ClickToCloseMessage`

---
+++ src/ui/components/universal/ClickToCloseMessage.tsx
@@ -0,0 +1,61 @@
+import { message } from 'antd';
+import { ArgsProps } from 'antd/lib/message';
+import { nanoid } from 'nanoid';
+
+function isArgsProps(jc: React.ReactNode | ArgsProps): jc is ArgsProps {
+  return typeof jc === 'object' && Boolean((jc as any).content);
+}
+
+type MessageParams = Parameters<typeof message['info']>;
+
+const createCTCMessage =
+  (name: 'info' | 'success' | 'error' | 'warning' | 'loading') =>
+  (...params: MessageParams) => {
+    let content = params[0] as ArgsProps;
+    if (!isArgsProps(params[0])) {
+      content = {
+        content: params[0],
+      } as ArgsProps;
+    }
+
+    let msgKey: string | number;
+    if (!content.key) {
+      /**
+       * Generate a key for onClick to close
+       */
+      msgKey = nanoid();
+      content.key = msgKey;
+    } else {
+      msgKey = content.key;
+    }
+
+    content.onClick = (e: React.MouseEvent<HTMLDivElement>) => {
+      if (content.onClick) {
+        /**
+         * continute if onClick existed
+         */
+        content.onClick(e);
+      }
+      message.destroy(msgKey);
+    };
+    params[0] = content;
+    message[name](...params);
+  };
+
+export const ClickToCloseMessage = {
+  info: (...params: Parameters<typeof message['info']>) => {
+    return createCTCMessage('info')(...params);
+  },
+  success: (...params: MessageParams) => {
+    return createCTCMessage('success')(...params);
+  },
+  error: (...params: MessageParams) => {
+    return createCTCMessage('error')(...params);
+  },
+  warning: (...params: MessageParams) => {
+    return createCTCMessage('warning')(...params);
+  },
+  loading: (...params: MessageParams) => {
+    return createCTCMessage('loading')(...params);
+  },
+};
