a06b1c56cd2bc04f53d738a7f4899c32482f267b
Frank Wei <frank.wei@mirana.tech>
2022-02-22 18:45:35 +0800

    feat(tx-action): `cancelTx` and `cancelTxs` to cancel tx

--- src/ui/state/actions.ts
+++ src/ui/state/actions.ts
@@ -1,5 +1,21 @@
-import { addHexPrefix } from 'background/utils/lib-util';
-import { Dispatch } from 'redux';
+import { addHexPrefix, getEnvironmentType } from 'background/utils/lib-util';
+import { ENVIRONMENT_TYPE_NOTIFICATION } from 'constants/app';
+import { AnyAction, Dispatch } from 'redux';
+import { ThunkDispatch } from 'redux-thunk';
+import txHelper from 'ui/helpers/utils/tx-helper';
+import {
+  hideLoadingIndicator,
+  showLoadingIndicator,
+} from 'ui/reducer/appState.reducer';
+import { getCurrentChainId } from 'ui/selectors/selectors';
+import {
+  unapprovedMsgsSelector,
+  unapprovedTxsSelector,
+  unapprovedPersonalMsgsSelector,
+  unapprovedDecryptMsgsSelector,
+  unapprovedEncryptionPublicKeyMsgsSelector,
+  unapprovedTypedMessagesSelector,
+} from 'ui/selectors/transactions';
 import { RootState } from '../reducer';
 import { getMethodDataAsync } from '../utils/transactions';
 
@@ -8,7 +24,7 @@ export function getContractMethodData(
   provider: any,
   background: any
 ) {
-  return (dispatch: Dispatch, getState: () => RootState) => {
+  return (_: Dispatch, getState: () => RootState) => {
     const prefixedData: string = addHexPrefix(data);
     const fourBytePrefix = prefixedData.slice(0, 10);
     const knownMethodData = getState().knownMethod;
@@ -40,5 +56,114 @@ export function getContractMethodData(
         return { name, params };
       }
     );
+  };
+}
+
+export function cancelTx(
+  txData: { id: string },
+  _showLoadingIndication = true,
+  background: any
+) {
+  return (dispatch: ThunkDispatch<RootState, void, AnyAction>) => {
+    _showLoadingIndication && dispatch(showLoadingIndicator());
+    return new Promise<void>((resolve, reject) => {
+      background.cancelTransaction(txData.id, (error) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+
+        resolve();
+      });
+    })
+      .then(() => {
+        // dispatch(resetSendState());
+        dispatch(completedTx(txData.id));
+        dispatch(hideLoadingIndicator());
+        // @todo:
+        // dispatch(closeCurrentNotificationWindow());
+
+        return txData;
+      })
+      .catch((error) => {
+        dispatch(hideLoadingIndicator());
+        throw error;
+      });
+  };
+}
+
+/**
+ * Cancels all of the given transactions
+ *
+ * @param txDataList - a list of tx data objects
+ * @returns {function(*): Promise<void>}
+ */
+export function cancelTxs(txDataList: { id: string }[], background: any) {
+  return async (dispatch: ThunkDispatch<RootState, void, AnyAction>) => {
+    dispatch(showLoadingIndicator());
+
+    try {
+      const txIds = txDataList.map(({ id }) => id);
+      const cancellations = txIds.map(
+        (id) =>
+          new Promise<void>((resolve, reject) => {
+            background.cancelTransaction(id, (err) => {
+              if (err) {
+                reject(err);
+                return;
+              }
+
+              resolve();
+            });
+          })
+      );
+
+      await Promise.all(cancellations);
+
+      // @todo:
+      // dispatch(resetSendState());
+
+      txIds.forEach((id) => {
+        dispatch(completedTx(id));
+      });
+    } finally {
+      if (getEnvironmentType() === ENVIRONMENT_TYPE_NOTIFICATION) {
+        // @todo:
+        // closeNotificationPopup();
+      } else {
+        dispatch(hideLoadingIndicator());
+      }
+    }
+  };
+}
+
+export function completedTx(id: string) {
+  return (
+    dispatch: ThunkDispatch<RootState, void, AnyAction>,
+    getState: () => RootState
+  ) => {
+    const state = getState();
+    const chainId = getCurrentChainId(state);
+
+    const unconfirmedActions = txHelper(
+      unapprovedTxsSelector(state),
+      unapprovedMsgsSelector(state),
+      unapprovedPersonalMsgsSelector(state),
+      unapprovedDecryptMsgsSelector(state),
+      unapprovedEncryptionPublicKeyMsgsSelector(state),
+      unapprovedTypedMessagesSelector(state),
+      chainId
+    );
+    const otherUnconfirmedActions = unconfirmedActions.filter(
+      (tx) => tx.id !== id
+    );
+    dispatch({
+      // actionConstant.COMPLETED_TX
+      type: 'COMPLETED_TX',
+      value: {
+        id,
+        unconfirmedActionsCount: otherUnconfirmedActions.length,
+      },
+    });
   };
 }
