5bab4e5b74c052ec09f239e90579a58d9af38253
Frank Wei <frank.wei@mirana.tech>
2022-02-23 16:46:21 +0800

    refactor(network): move some stuff to `NetworkSelectionItem`

---
+++ src/ui/components/Network/NetworkSelection/NetworkSelectionItem.component.tsx
@@ -0,0 +1,134 @@
+import { Modal } from 'antd';
+import clsx from 'clsx';
+import React, { useCallback, useContext, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { Provider } from 'types/network';
+import { IconButton } from 'ui/components/IconButton';
+import { ClickToCloseMessage } from 'ui/components/universal/ClickToCloseMessage';
+import { NetworkProviderContext } from 'ui/context/NetworkProvider';
+import IconCheck from 'assets/action-icon/check.svg';
+import IconTrash from 'assets/action-icon/trash.svg';
+import IconEdit from 'assets/action-icon/edit.svg';
+
+type NetworkProviderWithOptionalTag = Provider & { idx?: number };
+
+interface NetworkSelectionItemProps {
+  network: NetworkProviderWithOptionalTag;
+}
+
+export function NetworkSelectionItem({ network }: NetworkSelectionItemProps) {
+  /**
+   * Some data source hooks
+   */
+  const providerContext = useContext(NetworkProviderContext);
+  const history = useHistory();
+  const currentProviderId = useSelector((s) => s.network.provider.id);
+  const isSelectedNetwork = useMemo(
+    () => network.id === currentProviderId,
+    [network, currentProviderId]
+  );
+
+  const selectProvider = useCallback(
+    (network: NetworkProviderWithOptionalTag) => {
+      console.debug(`Selected Chain ${network.chainId}`);
+      if (network.type === 'rpc') {
+        providerContext?.useCustomProvider(network.idx as number);
+      } else {
+        providerContext?.usePresetProvider(network.type);
+      }
+      // jump back previous page
+      history.goBack();
+    },
+    [providerContext, history]
+  );
+  return (
+    <div
+      key={network.chainId}
+      className={clsx('flex items-center network-item', {
+        'network-item-active': isSelectedNetwork,
+      })}
+      onClick={() => selectProvider(network)}
+    >
+      <span className="network-name">{network.nickname}</span>
+      <NetworkActions network={network} />
+    </div>
+  );
+}
+
+const NetworkActions = ({
+  network,
+}: {
+  network: NetworkProviderWithOptionalTag;
+}) => {
+  const currentProviderId = useSelector((s) => s.network.provider.id);
+  const isSelectedNetwork = useMemo(() => {
+    return network.id === currentProviderId;
+  }, [network, currentProviderId]);
+  return (
+    <span className="actions flex justify-center items-center">
+      {isSelectedNetwork ? (
+        <IconButton icon={IconCheck} size={12} />
+      ) : (
+        <div className="actions-not-selected">
+          <RpcNetworkOptions network={network} />
+        </div>
+      )}
+    </span>
+  );
+};
+
+const RpcNetworkOptions = ({
+  network,
+}: {
+  network: NetworkProviderWithOptionalTag;
+}) => {
+  const providerContext = useContext(NetworkProviderContext);
+  const { t } = useTranslation();
+  const history = useHistory();
+
+  const handleRemove = useCallback(
+    (e: React.MouseEvent<HTMLDivElement>) => {
+      // stop the parent's onClick event
+      e.stopPropagation();
+      Modal.confirm({
+        title: t('Do you want to delete this network?'),
+        content: t('You might have to add this back, are you sure?'),
+        onOk: async () => {
+          await providerContext?.removeCustomProvider(network.idx as number);
+          ClickToCloseMessage.success(t('remove_custom_provider_success'));
+        },
+        onCancel: () => {
+          ClickToCloseMessage.info(t('remove_custom_provider_cancel'));
+        },
+      });
+    },
+    [providerContext]
+  );
+
+  if (!providerContext) {
+    return <p>Loading</p>;
+  }
+
+  if (network.type !== 'rpc') {
+    // Only `rpc` type are editable
+    // other type was preset that cannot edit
+    return null;
+  }
+
+  return (
+    <div className="flex justify-center items-center">
+      <IconButton icon={IconTrash} size={12} onClick={handleRemove} />
+      <IconButton
+        icon={IconEdit}
+        size={12}
+        onClick={(e) => {
+          // stop the parent's onClick event
+          e.stopPropagation();
+          history.push(`/network/edit/${network.idx}`);
+        }}
+      />
+    </div>
+  );
+};
--- src/ui/views/Network/NetworksSelectionPage.tsx
+++ src/ui/views/Network/NetworksSelectionPage.tsx
@@ -1,33 +1,19 @@
-import React, {
-  Fragment,
-  useCallback,
-  useContext,
-  useEffect,
-  useMemo,
-} from 'react';
+import React, { Fragment, useContext, useEffect, useMemo } from 'react';
 import { useHistory } from 'react-router';
 import { useTranslation } from 'react-i18next';
 import { NetworksCategories } from './typing';
-import IconCheck from 'assets/action-icon/check.svg';
-import IconTrash from 'assets/action-icon/trash.svg';
-import IconEdit from 'assets/action-icon/edit.svg';
 import DefaulutIcon from 'assets/tokens/default.svg';
 import './style.less';
-import { IconButton } from 'ui/components/IconButton';
 import { NetworkProviderContext } from 'ui/context/NetworkProvider';
-import { defaultNetworks, PresetNetworkId } from 'constants/defaultNetwork';
-
+import { defaultNetworks } from 'constants/defaultNetwork';
 import GeneralHeader from 'ui/components/Header/GeneralHeader';
-import { Provider } from 'types/network';
-import { BigNumber } from '@ethersproject/bignumber';
-import { Modal, message, Button } from 'antd';
+import { Button } from 'antd';
 import { categoryToIconSVG } from 'ui/utils/networkCategoryToIcon';
 import { useSelector } from 'react-redux';
 import { IconComponent } from 'ui/components/IconComponents';
-import { ClickToCloseMessage } from 'ui/components/universal/ClickToCloseMessage';
-import clsx from 'clsx';
+import { NetworkSelectionItem } from 'ui/components/Network/NetworkSelection/NetworkSelectionItem.component';
 
-const Icon = (src: string) => (
+const ChainCategoryIcon = ({ src = DefaulutIcon }: { src?: string }) => (
   <img
     style={{
       width: '24px',
@@ -75,114 +61,11 @@ function useProviderList() {
   return networkList;
 }
 
-type NetworkProviderWithOptionalTag = Provider & { idx?: number };
-
-const RpcNetworkOptions = ({
-  network,
-}: {
-  network: NetworkProviderWithOptionalTag;
-}) => {
-  const providerContext = useContext(NetworkProviderContext);
-  const { t } = useTranslation();
-  const history = useHistory();
-
-  const handleRemove = useCallback(
-    (e: React.MouseEvent<HTMLDivElement>) => {
-      // stop the parent's onClick event
-      e.stopPropagation();
-      Modal.confirm({
-        title: t('Do you want to delete this network?'),
-        content: t('You might have to add this back, are you sure?'),
-        onOk: async () => {
-          await providerContext?.removeCustomProvider(network.idx as number);
-          ClickToCloseMessage.success(t('remove_custom_provider_success'));
-        },
-        onCancel: () => {
-          ClickToCloseMessage.info(t('remove_custom_provider_cancel'));
-        },
-      });
-    },
-    [providerContext]
-  );
-
-  if (!providerContext) {
-    return <p>Loading</p>;
-  }
-
-  if (network.type !== 'rpc') {
-    // Only `rpc` type are editable
-    // other type was preset that cannot edit
-    return null;
-  }
-
-  return (
-    <div className="flex justify-center items-center">
-      <IconButton icon={IconTrash} size={12} onClick={handleRemove} />
-      <IconButton
-        icon={IconEdit}
-        size={12}
-        onClick={(e) => {
-          // stop the parent's onClick event
-          e.stopPropagation();
-          history.push(`/network/edit/${network.idx}`);
-        }}
-      />
-    </div>
-  );
-};
-
-const NetworkActions = ({
-  network,
-}: {
-  network: NetworkProviderWithOptionalTag;
-}) => {
-  const currentNetworkController = useSelector((state) => state.network);
-  const isSelectedNetwork = useMemo(() => {
-    return BigNumber.from(network.chainId).eq(
-      currentNetworkController?.provider.chainId || '0'
-    );
-  }, [network, currentNetworkController]);
-  return (
-    <span className="actions flex justify-center items-center">
-      {isSelectedNetwork ? (
-        <IconButton icon={IconCheck} size={12} />
-      ) : (
-        <div className="actions-not-selected">
-          <RpcNetworkOptions network={network} />
-        </div>
-      )}
-    </span>
-  );
-};
-
 const NetworksSelectionContainer = () => {
   const { t } = useTranslation();
   const history = useHistory();
   const providerContext = useContext(NetworkProviderContext);
-  const currentProviderId = useSelector((s) => s.network.provider.id);
   const networkList = useProviderList();
-
-  useEffect(() => {
-    if (providerContext)
-      console.debug(
-        'providerContext.currentNetworkController:',
-        providerContext.currentNetworkController
-      );
-  }, [providerContext]);
-
-  const selectProvider = useCallback(
-    (network: NetworkProviderWithOptionalTag) => {
-      console.debug(`Selected Chain ${network.chainId}`);
-      if (network.type === 'rpc') {
-        providerContext?.useCustomProvider(network.idx as number);
-      } else {
-        providerContext?.usePresetProvider(network.type);
-      }
-      // jump back previous page
-      history.goBack();
-    },
-    [providerContext, history]
-  );
 
   if (!providerContext) {
     return <p>Loading...</p>;
@@ -201,30 +84,17 @@ const NetworksSelectionContainer = () => {
           return (
             <div className="networklist-category" key={key}>
               <div className="category-tag flex items-center">
-                {Icon(currentCategory.icon || DefaulutIcon)}
+                <ChainCategoryIcon src={currentCategory.icon} />
                 <h2 className="category-name">{currentCategory.displayName}</h2>
               </div>
-              {currentCategory.networks.map((network) => {
-                const isSelectedNetwork = network.id === currentProviderId;
-                return (
-                  <div
-                    key={network.chainId}
-                    className={clsx('flex items-center network-item', {
-                      'network-item-active': isSelectedNetwork,
-                    })}
-                    onClick={() => selectProvider(network)}
-                  >
-                    <span className="network-name">{network.nickname}</span>
-                    <NetworkActions network={network} />
-                  </div>
-                );
-              })}
+              {currentCategory.networks.map((network) => (
+                <NetworkSelectionItem network={network} key={network.id} />
+              ))}
             </div>
           );
         })}
       </div>
       <div
-        key="customize"
         className="cursor-pointer hover-to-highlight custom-network-card flex items-center"
         onClick={() => history.push('/network/add')}
       >
