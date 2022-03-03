85ccbec16ab0f5a1e4661bceb32edad5a8c90202
Frank Wei <frank.wei@mirana.tech>
2022-03-03 10:51:55 +0800

    chore(tx): make `GAS_ESTIMATE_TYPES` enum

--- src/constants/gas.ts
+++ src/constants/gas.ts
@@ -16,11 +16,11 @@ export const GAS_LIMITS = {
  * These are already declared in @metamask/controllers but importing them from
  * that module and re-exporting causes the UI bundle size to expand beyond 4MB
  */
-export const GAS_ESTIMATE_TYPES = {
-  FEE_MARKET: 'fee-market',
-  LEGACY: 'legacy',
-  ETH_GASPRICE: 'eth_gasPrice',
-  NONE: 'none',
+export enum GAS_ESTIMATE_TYPES {
+  FEE_MARKET = 'fee-market',
+  LEGACY = 'legacy',
+  ETH_GASPRICE = 'eth_gasPrice',
+  NONE = 'none',
 };
 
 /**
