a1b70722a0098587b85d70e8ee7ca805939ea352
marvelfisher@gmail.com <marvelfisher@gmail.com>
2022-04-14 14:08:20 +0800

    feat: skynet refine product config to avoid upload data into test project

--- src/utils/skynet.ts
+++ src/utils/skynet.ts
@@ -5,14 +5,14 @@ const start = () => {
     {
       show_log: false,
       server_url: `https://sc-datasink.ffe390afd658c19dcbf707e0597b846d.de/sa?project=${
-        process.env.ENV === 'prod' ? 'production' : 'default'
+        process.env.BUILD_ENV === 'PRO' ? 'production' : 'default'
       }`,
       abUrl:
         'https://sc-abtest.ffe390afd658c19dcbf707e0597b846d.de/api/v2/abtest/online/results?project-key=04998C9B84A38A7D810A5ADAF48229BA568BF954',
     },
     {
       app_id: 10009,
-      env: process.env.ENV,
+      env: process.env.BUILD_ENV,
       project_type: 'teleport',
       project_name: 'teleport_wallet',
       page_name: document && document.title,
