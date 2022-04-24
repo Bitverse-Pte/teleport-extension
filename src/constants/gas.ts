786cda20344c0548f21a3a5652cd875cf5e0b2bf
Frank Wei <frank.wei@mirana.tech>
2022-04-24 16:01:55 +0800

    feat(tx): basic tx gas fee validation utils

--- src/constants/gas.ts
+++ src/constants/gas.ts
@@ -95,7 +95,9 @@ export function getGasFormErrorText(
 ) {
   switch (type) {
     case GAS_FORM_ERRORS.GAS_LIMIT_OUT_OF_BOUNDS:
-      return t('editGasLimitOutOfBounds', [minimumGasLimit]);
+      return t('editGasLimitOutOfBounds', {
+        replace: { $1: minimumGasLimit },
+      });
     case GAS_FORM_ERRORS.MAX_PRIORITY_FEE_TOO_LOW:
       return t('editGasMaxPriorityFeeLow');
     case GAS_FORM_ERRORS.MAX_FEE_TOO_LOW:
