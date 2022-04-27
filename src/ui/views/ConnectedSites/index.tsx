22a244be7cc2c9bfb11b9546e04bf2dd4dd8237d
marvelfisher@gmail.com <marvelfisher@gmail.com>
2022-04-27 14:13:47 +0800

    feat: more sensor event for account switch and connected sites page

--- src/ui/views/ConnectedSites/index.tsx
+++ src/ui/views/ConnectedSites/index.tsx
@@ -17,7 +17,8 @@ import accountSwitch from 'assets/accountSwitch.svg';
 import { useTranslation } from 'react-i18next';
 import { ConnectedSite } from 'background/service/permission';
 import { NoContent } from 'ui/components/universal/NoContent';
-
+import skynet from 'utils/skynet';
+const { sensors } = skynet;
 interface IConnectedSitesProps {
   account?: BaseAccount;
   visible?: boolean;
@@ -69,8 +70,16 @@ const ConnectedSites: React.FC<IConnectedSitesProps> = (
         alignItems: 'center',
       },
       async onOk() {
+        sensors.track('teleport_connected_sites_confirm_ok', {
+          page: location.pathname,
+        });
         await wallet.removeConnectedSite(site.origin, account?.address);
         init();
+      },
+      async onCancel() {
+        sensors.track('teleport_connected_sites_confirm_cancel', {
+          page: location.pathname,
+        });
       },
     });
   };
@@ -100,6 +109,9 @@ const ConnectedSites: React.FC<IConnectedSitesProps> = (
             onClick={(e) => {
               e.stopPropagation();
               if (props.handleOnClose) {
+                sensors.track('teleport_connected_sites_switch', {
+                  page: location.pathname,
+                });
                 props.handleOnClose();
               }
             }}
@@ -130,6 +142,10 @@ const ConnectedSites: React.FC<IConnectedSitesProps> = (
                 className="site-item-right cursor flexR"
                 onClick={(e) => {
                   e.stopPropagation();
+                  sensors.track('teleport_connected_sites_remove', {
+                    page: location.pathname,
+                    site: a.origin,
+                  });
                   handleRemoveSiteClick(a);
                 }}
               >
