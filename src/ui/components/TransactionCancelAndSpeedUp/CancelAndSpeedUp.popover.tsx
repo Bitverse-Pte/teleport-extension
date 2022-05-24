3ac1976e08550593b3d364961da3593646250180
Frank Wei <frank.wei@davionlabs.com>
2022-05-24 12:01:55 +0800

    refactor(speedUp): split new component `CustomGasInput`

--- src/ui/components/TransactionCancelAndSpeedUp/CancelAndSpeedUp.popover.tsx
+++ src/ui/components/TransactionCancelAndSpeedUp/CancelAndSpeedUp.popover.tsx
@@ -25,6 +25,7 @@ import { ClickToCloseMessage } from 'ui/components/universal/ClickToCloseMessage
 import { MIN_GAS_LIMIT_DEC } from 'ui/context/send.constants';
 import { usePrevious, useSetState } from 'react-use';
 import { IconComponent } from '../IconComponents';
+import { CustomGasInput } from './component/CustomGasInput.component';
 
 interface CancelAndSpeedUpPopoverParams {
   editGasMode: EDIT_GAS_MODES;
@@ -180,18 +181,14 @@ const CancelSpeedupPopoverImplementation = ({
   );
   const previousSelectedGasTier = usePrevious(selectedGasTier);
 
-  const [customGasPrice, setCustomGasPrice] = useSetState<
-    Partial<Transaction['txParams']>
-  >({
+  const gasPriceuseSetState = useSetState<Partial<Transaction['txParams']>>({
     estimateUsed: PRIORITY_LEVELS.CUSTOM,
     maxFeePerGas: add10PercentTxParams.maxFeePerGas,
     maxPriorityFeePerGas: add10PercentTxParams.maxPriorityFeePerGas,
     gasPrice: add10PercentTxParams.gasPrice,
   });
 
-  const [customTxParamsError, setCustomTxParamsError] = useSetState<
-    Partial<Transaction['txParams']>
-  >({});
+  const [customGasPrice] = gasPriceuseSetState;
 
   const shouldDrawerExpanded = selectedGasTier === PRIORITY_LEVELS.CUSTOM;
 
@@ -419,124 +416,13 @@ const CancelSpeedupPopoverImplementation = ({
           </div>
         </div>
 
-        <div
-          className="flex items-center flex-col custom-gas"
-          style={{ marginTop: 4 }}
-        >
-          {selectedGasTier === PRIORITY_LEVELS.CUSTOM && (
-            <div className="field">
-              <h1 className="form-title bold">Gas Limit</h1>
-              <InputNumber<string>
-                style={{ width: '100% ' }}
-                stringMode
-                value={Number(gasLimit || 21000).toString()}
-                min="21000"
-                controls={false}
-                onBlur={({ target }) => {
-                  try {
-                    setGasLimit(BigNumber.from(target.value).toHexString());
-                  } catch (error) {
-                    console.error('setGasLimit::error:', error);
-                    setCustomTxParamsError({
-                      gasLimit: 'bad input',
-                    });
-                  }
-                }}
-              />
-            </div>
-          )}
-          {!isEIP1559Tx && selectedGasTier === PRIORITY_LEVELS.CUSTOM && (
-            <div className="field">
-              <h1 className="form-title bold">Gas Price</h1>
-              <InputNumber<string>
-                style={{ width: '100% ' }}
-                addonAfter="Gwei"
-                // onChange={onChange}
-                stringMode
-                value={utils.formatUnits(
-                  customGasPrice.gasPrice || '0',
-                  'gwei'
-                )}
-                controls={false}
-                onBlur={({ target: { value } }) => {
-                  try {
-                    const gasPrice = utils
-                      .parseUnits(value, 'gwei')
-                      .toHexString();
-                    setCustomGasPrice({
-                      gasPrice,
-                    });
-                  } catch (error) {
-                    console.error('setCustomGasPrice::error', error);
-                    setCustomTxParamsError({
-                      gasPrice: 'bad input',
-                    });
-                  }
-                }}
-              />
-            </div>
-          )}
-          {isEIP1559Tx && selectedGasTier === PRIORITY_LEVELS.CUSTOM && (
-            <div className="field">
-              <h1 className="form-title bold">Max Fee</h1>
-              <InputNumber<string>
-                style={{ width: '100% ' }}
-                value={utils.formatUnits(
-                  customGasPrice.maxFeePerGas || '0',
-                  'gwei'
-                )}
-                controls={false}
-                onBlur={({ target: { value } }) => {
-                  try {
-                    const maxFeePerGas = utils
-                      .parseUnits(value, 'gwei')
-                      .toHexString();
-                    setCustomGasPrice({
-                      maxFeePerGas,
-                    });
-                  } catch (error) {
-                    console.error('setCustomGasPrice::error', error);
-                    setCustomTxParamsError({
-                      maxFeePerGas: 'bad input',
-                    });
-                  }
-                }}
-                addonAfter="Gwei"
-                stringMode
-              />
-            </div>
-          )}
-          {isEIP1559Tx && selectedGasTier === PRIORITY_LEVELS.CUSTOM && (
-            <div className="field">
-              <h1 className="form-title bold">Max Priority Fee</h1>
-              <InputNumber<string>
-                style={{ width: '100% ' }}
-                value={utils.formatUnits(
-                  customGasPrice.maxPriorityFeePerGas || '0',
-                  'gwei'
-                )}
-                controls={false}
-                onBlur={({ target: { value } }) => {
-                  try {
-                    const maxPriorityFeePerGas = utils
-                      .parseUnits(value, 'gwei')
-                      .toHexString();
-                    setCustomGasPrice({
-                      maxPriorityFeePerGas,
-                    });
-                  } catch (error) {
-                    console.error('setCustomGasPrice::error', error);
-                    setCustomTxParamsError({
-                      maxPriorityFeePerGas: 'bad input',
-                    });
-                  }
-                }}
-                addonAfter="Gwei"
-                stringMode
-              />
-            </div>
-          )}
-        </div>
+        <CustomGasInput
+          gasLimit={gasLimit}
+          setGasLimit={setGasLimit}
+          gasPriceuseSetState={gasPriceuseSetState}
+          selectedGasTier={selectedGasTier}
+          isEIP1559Tx={isEIP1559Tx}
+        />
       </div>
       <Button
         type="primary"
