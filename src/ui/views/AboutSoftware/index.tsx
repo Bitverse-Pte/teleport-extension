67e9ff7652ccfb4500e0fdfca220371a8b3a83cb
FrankFan <fanyong@gmail.com>
2022-12-18 00:08:08 +0800

    feat: 修改 About 页,增加二维码,修改官网地址,修改样式

--- src/ui/views/AboutSoftware/index.tsx
+++ src/ui/views/AboutSoftware/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from 'ui/components/Header';
-import TeleportLogoURI from 'assets/logo.svg';
+import BitverseLogoImg from 'assets/logo.svg';
+import QrCodeImg_Android from 'assets/qc_android.png';
+import QrCodeImg_IOS from 'assets/qc_ios.png';
 import './style.less';
 import { useTranslation } from 'react-i18next';
 import { IconComponent } from 'ui/components/IconComponents';
@@ -14,19 +16,15 @@ export default function AboutSoftware() {
 
   const { isDarkMode } = useDarkmode();
 
-  const onTeleportHomePage = () => {
-    window.open('https://teleport.network');
+  const goToWWW = () => {
+    window.open('https://www.bitverse.zone');
   };
 
   return (
     <div className={clsx('about-software', { dark: isDarkMode })}>
       <Header title="About" />
       <div className="page-container flex flex-col items-center">
-        <img
-          src={TeleportLogoURI}
-          alt="Teleport Network Logo"
-          className="logo"
-        />
+        <img src={BitverseLogoImg} alt="Bitverse Logo" className="logo" />
         <TlpTextLogo className="logotext" viewBox="0 0 102 13" />
         <p className="software-version flexR">V {process.env.version}</p>
         <p className="software-introductions">
@@ -38,15 +36,25 @@ export default function AboutSoftware() {
         </p>
         <div
           className="jump-to-home-page cursor-pointer flex"
-          onClick={onTeleportHomePage}
+          onClick={goToWWW}
         >
-          <img
-            src={TeleportLogoURI}
-            alt="Teleport Network Logo"
-            className="web-icon"
-          />
+          <img src={BitverseLogoImg} alt="Bitverse Logo" className="web-icon" />
           <span className="url">https://bitverse.zone/</span>
           <IconComponent name="chevron-right" cls="ml-auto" />
+        </div>
+        <div className="about-qrcode-container">
+          <div className="qrcode_box">
+            <div className="qrcode_box_title">for Android</div>
+            <img
+              src={QrCodeImg_Android}
+              alt="android"
+              className="about-qrcode"
+            />
+          </div>
+          <div className="qrcode_box">
+            <div className="qrcode_box_title">for iOS</div>
+            <img src={QrCodeImg_IOS} alt="ios" className="about-qrcode" />
+          </div>
         </div>
       </div>
     </div>
